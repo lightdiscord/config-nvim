a1f0ec48249ecf47d5078365f1edf3636b6ce1cf
LightDiscord <root@arnaud.sh>
2020-03-24 15:00:11 +0100

    feat: use gitsubmodule instead of nix

--- default.nix
+++ default.nix
@@ -1,23 +1,3 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-with pkgs.lib;
-
-let
-	build = { owner, repo, rev, sha256, name ? repo, ... }: pkgs.vimUtils.buildVimPluginFrom2Nix {
-		inherit name;
-
-		src = pkgs.fetchFromGitHub {
-			fetchSubmodules = true;
-
-			inherit name owner repo rev sha256;
-		};
-	};
-in pkgs.neovim.override {
-	withNodeJs = true;
-
-	configure = {
-		customRC = builtins.readFile ./src/init.vim;
-
-		packages.myVimPackage.start = map build (import ./plugins);
-	};
+{
+	home.file.".config/nvim".source = ./src;
 }
